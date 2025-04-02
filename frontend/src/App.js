be02992fee1d7219665058558d2c0ba85bccf6e6
Rodolfo Espinosa-Nunez <rodolfoespinosa01@gmail.com>
2025-04-02 18:48:52 -0400

    reorganized files, created home page, user page, user selection plan, new folders one for admin and one for user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminHomePage from './pages/AdminHomePage';
-import AdminPlanSelectionPage from './pages/AdminPlanSelectionPage'; // 👈 NEW
+import MainHomePage from './pages/MainHomePage';
+import AdminHomePage from './pages/admin/AdminHomePage';
+import UserHomePage from './pages/user/UserHomePage';
+import AdminPlanSelectionPage from './pages/admin/AdminPlanSelectionPage';
+import UserPlanSelectionPage from './pages/user/UserPlanSelectionPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AdminHomePage />} />
+        <Route path="/" element={<MainHomePage />} />
+        <Route path="/adminhomepage" element={<AdminHomePage />} />
+        <Route path="/userhomepage" element={<UserHomePage />} />
         <Route path="/adminplans" element={<AdminPlanSelectionPage />} />
+        <Route path="/userplans" element={<UserPlanSelectionPage />} />
       </Routes>
     </Router>
   );
