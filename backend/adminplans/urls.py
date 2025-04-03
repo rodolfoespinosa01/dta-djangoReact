989aa5bf454e2ce03d12c9ee72ed325abd2d2919
Rodolfo Espinosa-Nunez <rodolfoespinosa01@gmail.com>
2025-04-03 14:59:55 -0400

    Small adjustment to login redirect to /admindashboard instead of /admin-dashboard

--- backend/adminplans/urls.py
+++ backend/adminplans/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import create_checkout_session 
+from .views import create_checkout_session
+from . import views
 
 urlpatterns = [
     path('create-checkout-session/', create_checkout_session),
+    path('register-admin/', views.register_admin, name='register_admin'),
 ]
