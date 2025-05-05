8fb90969738caded46bdcfbfb6bbd532b74f6f7c
Rodolfo Espinosa-Nunez <rodolfoespinosa01@gmail.com>
2025-05-05 00:47:21 -0400

    Still in the process of updatig the strucure of the project and working on making the test scripts succeed

--- backend/adminplans/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .admin_plan import AdminPlan
-from .admin_profile import AdminProfile
-from .admin_account_history import AdminAccountHistory
-from .admin_pending_signup import AdminPendingSignup
