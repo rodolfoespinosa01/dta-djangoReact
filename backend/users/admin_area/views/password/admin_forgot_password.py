8614e1feebfe2e5cc6e6bf9033ea9f9a1fc2819e
Rodolfo Espinosa-Nunez <rodolfoespinosa01@gmail.com>
2025-05-10 13:17:15 -0400

    Fixed the forgot password flow, it was off a bit due to old naming convention that was originally set up with the project, using the ADMIN prefix

--- backend/users/admin_area/views/password/admin_forgot_password.py
+++ backend/users/admin_area/views/password/admin_forgot_password.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from users.admin_area.serializers import ResetPasswordSerializer
+from users.admin_area.serializers.forgot_password_serializer import ForgotPasswordSerializer
 
-class ResetPasswordConfirmView(APIView):
+class AdminForgotPasswordView(APIView):
     def post(self, request):
-        serializer = ResetPasswordSerializer(data=request.data)
+        serializer = ForgotPasswordSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"detail": "Password has been reset successfully."})
+            return Response({"detail": "Password reset link sent (check terminal)."})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
