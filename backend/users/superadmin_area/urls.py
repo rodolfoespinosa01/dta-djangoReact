9d47afc64632306b106873dcb450ed54335dd7d3
Rodolfo Espinosa-Nunez <rodolfoespinosa01@gmail.com>
2026-02-20 04:10:40 -0500

    Started working on making adjustment for superadmin flow

--- backend/users/superadmin_area/urls.py
+++ backend/users/superadmin_area/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from users.superadmin_area.views import dashboard
-from users.superadmin_area.views.token_login import TokenObtainPairView
+from users.superadmin_area.views.analytics import analytics
+from users.superadmin_area.views.token_login import SuperAdminTokenObtainPairView
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('analytics/', analytics, name='analytics'),
+    path('login/', SuperAdminTokenObtainPairView.as_view(), name='login'),
 ]
