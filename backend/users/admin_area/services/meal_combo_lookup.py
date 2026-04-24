ed8c1cc45e6e5e13fd388140af19bd944ea6f293
Rodolfo Espinosa-Nunez <rodolfoespinosa01@gmail.com>
2026-04-24 11:25:05 -0400

    Got the latest commit to work again. Currently I realized the meal generation isnt working and i need to dig deeper into it.

--- backend/users/admin_area/services/meal_combo_lookup.py
+++ backend/users/admin_area/services/meal_combo_lookup.py
@@ -3,4 +3,7 @@ from core.services.meal_combo_lookup import (
     find_meal_combo_template_by_slots,
 )
 
-__all__ = ["find_meal_combo_template_by_slots", "find_meal_combo_id_by_slots"]
+__all__ = [
+    "find_meal_combo_id_by_slots",
+    "find_meal_combo_template_by_slots",
+]
--- backend/users/admin_area/utils/__init__.py
+++ backend/users/admin_area/utils/__init__.py
@@ -1,5 +1,5 @@
 from .log_PendingSignup import log_PendingSignup
 from .log_TransactionLog import log_TransactionLog
 from .log_Profile import log_Profile
-from .log_precheckout import log_PreCheckout
+from .log_Precheckout import log_PreCheckout
 from .log_EventTracker import log_EventTracker
